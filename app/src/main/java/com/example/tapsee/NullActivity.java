00962304d353eb677ad1f3b3fe610f487373717f
Boulanger <alexandre-boulanger@bbox.fr>
2022-03-17 10:00:20 +0100

    bouton configurés

---
+++ app/src/main/java/com/example/tapsee/NullActivity.java
@@ -0,0 +1,14 @@
+package com.example.tapsee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NullActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_null);
+    }
+}
