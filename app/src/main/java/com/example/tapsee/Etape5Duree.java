38db864d1c130bc26df7f4a900c95c3e95fec7b4
Anne-So <anne-sophie.jourlin@eleves.ec-nantes.fr>
2022-03-17 09:50:32 +0100

    ajout layouts etape 6

---
+++ app/src/main/java/com/example/tapsee/Etape5Duree.java
@@ -0,0 +1,14 @@
+package com.example.tapsee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Etape5Duree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_etape5_duree);
+    }
+}
