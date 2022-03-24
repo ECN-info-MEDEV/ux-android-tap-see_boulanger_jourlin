91d98e0364def72edba9be90583052e289823b17
Anne-So <anne-sophie.jourlin@eleves.ec-nantes.fr>
2022-03-24 10:53:42 +0100

    add activity 4

--- app/src/main/java/com/example/tapsee/MainActivity.java
+++ app/src/main/java/com/example/tapsee/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
      * Methode lançant l'activité permettant de choisir un film
      */
     public void launchFilmActivity(View view){
-        Intent intent = new Intent(this, Etape5Duree.class);
+        Intent intent = new Intent(this, Etape4Plateforme.class);
         startActivity(intent);
     }
 
